222f8fd4f7ce7932bb7a63c0ad4f6fc71f66e773
Namesh Kushantha <44038404+techkush@users.noreply.github.com>
2020-04-29 17:58:53 +0530

    Get current location, Map Markers, Camera Position, Map Styles

---
+++ lib/maps.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'dart:async';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapsDemo extends StatefulWidget {
+  @override
+  _MapsDemoState createState() => _MapsDemoState();
+}
+
+class _MapsDemoState extends State<MapsDemo> {
+  Completer<GoogleMapController> _controller = Completer();
+  static const LatLng _center = const LatLng(5.9971, 80.4901);
+  final Set<Marker> _markers = {};
+  LatLng _lastMapPosition = _center;
+  MapType _currentMapType = MapType.normal;
+  String address;
+
+  @override
+  void initState() {
+    getUserLocation();
+    super.initState();
+  }
+
+  static final CameraPosition _position = CameraPosition(
+      bearing: 0, target: LatLng(5.9971, 80.4901), tilt: 59.440, zoom: 11);
+
+  Future<void> _goToPosition() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition((_position)));
+  }
+
+  _onMapCreated(GoogleMapController controller) {
+    _controller.complete(controller);
+  }
+
+  _onCameraMove(CameraPosition position) {
+    _lastMapPosition = position.target;
+  }
+
+  _onMapTypeButtonPress() {
+    setState(() {
+      _currentMapType = _currentMapType == MapType.normal
+          ? MapType.satellite
+          : MapType.normal;
+    });
+  }
+
+  getUserLocation() async {
+    Position position = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    List<Placemark> placemarks = await Geolocator()
+        .placemarkFromCoordinates(position.latitude, position.longitude);
+    Placemark placemark = placemarks[0];
+    String completeAddress =
+        '${placemark.subThoroughfare} ${placemark.thoroughfare}, ${placemark.subLocality} ${placemark.locality}, ${placemark.subAdministrativeArea}, ${placemark.administrativeArea} ${placemark.postalCode}, ${placemark.country}';
+    print(completeAddress);
+    String formattedAddress = '${placemark.locality}, ${placemark.country}';
+    setState(() {
+      this.address = completeAddress;
+    });
+  }
+
+  _onAddMarkerButtonPressed() {
+    setState(() {
+      _markers.add(Marker(
+          markerId: MarkerId(_lastMapPosition.toString()),
+          position: _lastMapPosition,
+          infoWindow: InfoWindow(
+              title: 'This is a Title', snippet: 'This is a snippet'),
+          icon: BitmapDescriptor.defaultMarker));
+    });
+    print(_markers);
+  }
+
+  Widget button(Function function, IconData icon) {
+    return FloatingActionButton(
+      onPressed: function,
+      materialTapTargetSize: MaterialTapTargetSize.padded,
+      backgroundColor: Colors.blue,
+      child: Icon(
+        icon,
+        size: 36.0,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          GoogleMap(
+            padding: EdgeInsets.only(bottom: 100, left: 15),
+            initialCameraPosition: CameraPosition(target: _center, zoom: 11.0),
+            onMapCreated: _onMapCreated,
+            mapType: _currentMapType,
+            markers: _markers,
+            onCameraMove: _onCameraMove,
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 50, right: 16, left: 16),
+            child: Align(
+              alignment: Alignment.topRight,
+              child: Column(
+                children: <Widget>[
+                  button(_onMapTypeButtonPress, Icons.map),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  button(_onAddMarkerButtonPressed, Icons.add_location),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  button(_goToPosition, Icons.location_searching),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Text('$address')
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
